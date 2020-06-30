ef8aa82582ef8480f2862cdaaaa2f1bb91200eee
Meredith Bashert <merbashert@gmail.com>
2020-06-30 13:25:57 -0400

    Commit before revert

--- bingewatcher-back/controllers/shows.php
+++ bingewatcher-back/controllers/shows.php
@@ -5,19 +5,20 @@ header("Content-Type: application/json");
 include_once __DIR__ . '/../models/show.php';
 
 
+
 if($_REQUEST['action'] === 'index'){
     echo json_encode(Shows::all());
 } else if($_REQUEST['action'] === 'create') {
     $request_body = file_get_contents('php://input');
     $body_object = json_decode($request_body);
-    $new_show = new Show(null, $body_object->title, $body_object->image, $body_object->seasons, $body_object->currentEpisode);
+    $new_show = new Show(null, $body_object->omdb_id, $body_object->name, $body_object->image, $body_object->seasons, $body_object->current_episode);
     $all_shows = Shows::create($new_show);
 
     echo json_encode($all_shows);
 } else if($_REQUEST['action'] ==='update'){
     $request_body = file_get_contents('php://input');
     $body_object = json_decode($request_body);
-    $updated_show = new Show($_REQUEST['id'], $body_object->title, $body_object->image, $body_object->seasons, $body_object->currentEpisode);
+    $updated_show = new Show($_REQUEST['id'], $body_object->omdb_id, $body_object->name, $body_object->image, $body_object->seasons, $body_object->current_episode);
     $all_shows = Shows::update($updated_show);
 
     echo json_encode($all_shows);
--- bingewatcher-back/models/show.php
+++ bingewatcher-back/models/show.php
@@ -1,50 +1,53 @@
     <?php
-$dbconn = pg_connect('host=localhost dbname=binge');
+// $dbconn = pg_connect('host=localhost dbname=binge');
 
-// $dbconn = null;
-// if(getenv('DATABASE_URL')){
-//     $connectionConfig = parse_url(getenv('DATABASE_URL'));
-//     $host = $connectionConfig['host'];
-//     $user = $connectionConfig['user'];
-//     $password = $connectionConfig['pass'];
-//     $port = $connectionConfig['port'];
-//     $dbname = trim($connectionConfig['path'],'/');
-//     $dbconn = pg_connect(
-//         "host=".$host." ".
-//         "user=".$user." ".
-//         "password=".$password." ".
-//         "port=".$port." ".
-//         "dbname=".$dbname
-//     );
-// } else {
-//     $dbconn = pg_connect("host=localhost dbname=binge");
-// }
+
+$dbconn = null;
+if(getenv('DATABASE_URL')){
+    $connectionConfig = parse_url(getenv('DATABASE_URL'));
+    $host = $connectionConfig['host'];
+    $user = $connectionConfig['user'];
+    $password = $connectionConfig['pass'];
+    $port = $connectionConfig['port'];
+    $dbname = trim($connectionConfig['path'],'/');
+    $dbconn = pg_connect(
+        "host=".$host." ".
+        "user=".$user." ".
+        "password=".$password." ".
+        "port=".$port." ".
+        "dbname=".$dbname
+    );
+} else {
+    $dbconn = pg_connect("host=localhost dbname=binge");
+}
 
 class Show {
     public $id;
+    public $omdb_id;
     public $name;
     public $image;
     public $seasons = array();
-    public $currentEpisode;
-    public function __construct($id, $name, $image, $seasons, $currentEpisode){
+    public $current_episode;
+    public function __construct($id, $omdb_id, $name, $image, $seasons, $current_episode){
         $this->id = $id;
+        $this->omdb_id = $omdb_id;
         $this->name = $name;
         $this->image = $image;
         $this->seasons = $seasons;
-        $this->currentEpisode = $currentEpisode;
+        $this->current_episode = $current_episode;
     }
 }
 
 class Shows {
     static function create($show){
-        $query = "INSERT INTO shows (name, image, seasons, currentEpisode) VALUES ($1, $2, $3, $4)";
-        $query_params = array($show->name, $show->image, $show->seasons, $show->currentEpisode);
+        $query = "INSERT INTO shows (omdb_id, name, image, seasons, current_episode) VALUES ($1, $2, $3, $4, $5)";
+        $query_params = array($show->omdb_id, $show->name, $show->image, $show->seasons, $show->current_episode);
         pg_query_params($query, $query_params);
         return self::all();
     }
     static function update($updated_show){
-        $query = "UPDATE shows SET name = $1, image=$2, seasons=$3, currentEpisode=$4 WHERE id=$5";
-        $query_params = array($updated_show->name, $updated_show->image, $updated_show->seasons, $updated_show->currentEpisode, $updated_show->id);
+        $query = "UPDATE shows SET omdb_id = $1, name = $2, image=$3, seasons=$4, current_episode=$5 WHERE id=$6";
+        $query_params = array($updated_show->omdb_id, $updated_show->name, $updated_show->image, $updated_show->seasons, $updated_show->current_episode, $updated_show->id);
         pg_query_params($query,$query_params);
 
         return self::all();
@@ -59,16 +62,17 @@ class Shows {
     static function all(){
         $shows = array();
 
-        $results = pg_query("SELECT * FROM shows ORDER BY name ASC");
+        $results = pg_query("SELECT * FROM shows");
 
         $row_object = pg_fetch_object($results);
         while($row_object) {
             $new_show = new Show(
                 intval($row_object->id),
+                intval($row_object->omdb_id),
                 $row_object->name,
                 $row_object->image,
-                intval($row_object->seasons),
-                intval($row_object->currentepisode)
+                $row_object->seasons,
+                intval($row_object->current_episode)
             );
             $shows[] = $new_show;
 
